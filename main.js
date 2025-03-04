9060692364a162c979542d6aac28ca6faf91adbf
Arthur Gomes <rm560771@fiap.com.br>
2025-03-03 21:30:34 -0300

    Subindo código

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-let add = document.getElementById = "greenButton"
-
+let add = document.getElementById = "greenButton";
+function adicionarCompromisso() {
+    document.getElementById = ("titulo").inneText = "Olá meu amor, eu te amo";  
+}
