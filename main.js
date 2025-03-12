ac26369dbfa221fce5f7082217c113495ac62428
Arthur Gomes <arthurszaa7@icloud.com>
2025-03-11 23:21:23 -0300

    Não subir

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let body = document.querySelector('body');
+let light_button = document.getElementById('lightButton')
+
 function dark() {
     body.setAttribute('class', 'darkmode');
+    light_button.setAttribute('id', 'lightButton')
 }
 function light(){
     body.removeAttribute('class', 'darkmode');
